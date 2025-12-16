d5c703b538aab96088485fde2b96e220e878b992
Protik Biswas <protikbiswas100@microsoft.com>
2025-12-16 12:19:21 +0530

    fixing android build issue

--- example/index.js
+++ example/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import DateTimePickerDemo from './DateTimePickerDemo';
+import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => DateTimePickerDemo);
+AppRegistry.registerComponent(appName, () => App);
