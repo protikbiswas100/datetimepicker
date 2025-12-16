fa30950522987a38bd3581f3bcc6b4abd2b5bf10
Protik Biswas <protikbiswas100@microsoft.com>
2025-12-16 10:35:16 +0530

    Convert DateTimePickerDemo to proper Win32 application - remove UWP dependencies
    - Replaced App/MainPage UWP classes with clean Win32 entry point
    - Changed from UWP to Win32 application type in vcxproj
    - Updated to use ReactNativeAppBuilder instead of XAML framework
    - Converted to use Composition-based CppApp props (not UWP)
    - Added proper Win32 headers and resource files
    - Removed XAML, IDL, and manifest files
    - Clean C++ only codebase

--- example/windows/DateTimePickerDemo/pch.h
+++ example/windows/DateTimePickerDemo/pch.h
@@ -1,24 +1,17 @@
 #pragma once
 
+#include "targetver.h"
+
 #define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
 
-#include <hstring.h>
-#include <restrictederrorinfo.h>
-#include <unknwn.h>
 #include <windows.h>
-#include <CppWinRTIncludes.h>
-#include <VersionMacros.h>
-#include <winrt/Windows.ApplicationModel.Activation.h>
-#include <UI.Xaml.Controls.Primitives.h>
-#include <UI.Xaml.Controls.h>
-#include <UI.Xaml.Markup.h>
-#include <UI.Xaml.Navigation.h>
+#include <shellscalingapi.h>
+#include <pathcch.h>
 
+#include <winrt/Windows.Foundation.h>
 #include <winrt/Microsoft.ReactNative.h>
+#include <winrt/Microsoft.ReactNative.Composition.h>
+#include <winrt/Microsoft.UI.Composition.h>
 
-#include <winrt/Microsoft.UI.Xaml.Automation.Peers.h>
-#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
-#include <winrt/Microsoft.UI.Xaml.Controls.h>
-#include <winrt/Microsoft.UI.Xaml.Media.h>
-#include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
-using namespace winrt::Windows::Foundation;
+#include <wil/cppwinrt.h>
